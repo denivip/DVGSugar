52d33081de4fd33777380b1f58275f8bd922d816
IPv6 <ipv6@Nikolays-iMac.local>
2016-02-01 17:24:32 +0300

    More goodness

---
+++ NSString+Formatting.h
@@ -0,0 +1,32 @@
+//
+//  NSString+Metrics.h
+//  NineHundredSeconds
+//
+//  Created by IPv6 on 20/05/15.
+//  Copyright (c) 2015 DENIVIP Group. All rights reserved.
+//
+
+#ifndef NSStringFormatting_h
+#define NSStringFormatting_h
+
+@interface NSString (Formatting)
++(NSString *)timediffFromSeconds:(NSUInteger)seconds;
++(NSString *)durationFromSeconds:(NSUInteger)seconds;
++(NSString *)lengthFromMeters:(NSUInteger)meters;
++(NSString *)niceNumbFromCount:(NSUInteger)count;
++(NSString*)plurlFromCount:(NSUInteger)count with:(NSArray*)pluralforms;
+@end
+
+@interface NSDate (Formatting)
+
++ (NSDateFormatter *)dvg_dateFormatter;
++ (NSDateFormatter *)dvg_dateTimeFormatter;
++ (NSString*)dvg_DateSince:(NSDate *)dt;
++ (NSString*)dvg_NiceDatestampSince:(NSDate*)dt;
++ (NSString*)dvg_NiceTimestampWithDate:(NSDate *)date;
++ (NSString*)dvg_NiceTimesinceWithDate:(NSDate *)date;
++ (BOOL)isSameDayWithDate1:(NSDate*)date1 date2:(NSDate*)date2;
+
+@end
+
+#endif
---
+++ NSString+Formatting.m
@@ -0,0 +1,216 @@
+//
+//  NSString+Metrics.m
+//  NineHundredSeconds
+//
+//  Created by IPv6 on 20/05/15.
+//  Copyright (c) 2015 DENIVIP Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSString+Formatting.h"
+
+
+@implementation NSString (Formatting)
+
++(NSString *)niceNumbFromCount:(NSUInteger)count {
+//    - Hundred and below: ones 5, 28, 923
+//    - Thousands: Ks with one decimal 12.2K, 29K
+//    - 100 thousands: 123K, 200K, 987K with no decimals
+//    - Millions: ones with one decimal 1.7M, 2M, 12.5M
+//    NSLog(@"TEST: %d %@",3,[NSString niceNumbFromCount:3]);
+//    NSLog(@"TEST: %d %@",99,[NSString niceNumbFromCount:9]);
+//    NSLog(@"TEST: %d %@",888,[NSString niceNumbFromCount:888]);
+//    NSLog(@"TEST: %d %@",10034,[NSString niceNumbFromCount:10034]);
+//    NSLog(@"TEST: %d %@",10334,[NSString niceNumbFromCount:10334]);
+//    NSLog(@"TEST: %d %@",100034,[NSString niceNumbFromCount:100034]);
+//    NSLog(@"TEST: %d %@",1022034,[NSString niceNumbFromCount:1022034]);
+//    NSLog(@"TEST: %d %@",1122034,[NSString niceNumbFromCount:1122034]);
+//    2015-07-06 17:18:14.764 NineHundredSeconds[3171:853480] TEST: 3 3
+//    2015-07-06 17:18:14.764 NineHundredSeconds[3171:853480] TEST: 99 9
+//    2015-07-06 17:18:14.765 NineHundredSeconds[3171:853480] TEST: 888 888
+//    2015-07-06 17:18:14.765 NineHundredSeconds[3171:853480] TEST: 10034 10K
+//    2015-07-06 17:18:14.765 NineHundredSeconds[3171:853480] TEST: 10334 10.3K
+//    2015-07-06 17:18:14.766 NineHundredSeconds[3171:853480] TEST: 100034 100K
+//    2015-07-06 17:18:14.766 NineHundredSeconds[3171:853480] TEST: 1022034 1M
+//    2015-07-06 17:18:14.766 NineHundredSeconds[3171:853480] TEST: 1122034 1.1M
+    
+    if(count < 1000){
+        return [NSString stringWithFormat:@"%lu", (unsigned long)count];
+    }
+    if(count < 100000){
+        if((count%1000)<(1000/10)){
+            return [NSString stringWithFormat:@"%luK", (unsigned long)(count/1000.0f)];
+        }
+        return [NSString stringWithFormat:@"%.01fK", (double)(count/1000.0f)];
+    }
+    if(count < 1000000.0f){
+        return [NSString stringWithFormat:@"%luK", (unsigned long)(count/1000.0f)];
+    }
+    if((count%1000000)<(1000000/10)){
+        return [NSString stringWithFormat:@"%luM", (unsigned long)(count/1000000.0f)];
+    }
+    return [NSString stringWithFormat:@"%.01fM", (double)(count/1000000.0f)];
+}
+
++(NSString*)plurlFromCount:(NSUInteger)count with:(NSArray*)pluralforms
+{
+    return [NSString stringWithFormat:@"%@ %@", [NSString niceNumbFromCount:count], (count==1)?[pluralforms objectAtIndex:0]:[pluralforms objectAtIndex:0]];
+}
+
++(NSString *)durationFromSeconds:(NSUInteger)seconds{
+    NSUInteger durationInHours = seconds / 3600;
+    NSUInteger durationInMinutes = (seconds % 3600) / 60;
+    NSUInteger durationInRemainder = seconds % 60;
+    if (durationInHours > 24) {
+        NSUInteger days = durationInHours/24;
+        return [NSString stringWithFormat:@"%lu %@", (unsigned long)days, (days==1)?NSLocalizedString(@"day", nil):NSLocalizedString(@"days", nil)];
+    }else if (durationInHours > 0) {
+        return [NSString stringWithFormat:@"%lu:%02lu:%02lu", (unsigned long)durationInHours, (unsigned long)durationInMinutes, (unsigned long)durationInRemainder];
+    }
+    else{
+        return [NSString stringWithFormat:@"%02lu:%02lu", (unsigned long)durationInMinutes, (unsigned long)durationInRemainder];
+    }
+}
+
++(NSString *)lengthFromMeters:(NSUInteger)meters{
+    NSUInteger lengthInKilometers = meters / 1000;
+    NSUInteger lengthInRemainder = meters % 1000;
+    if (lengthInKilometers > 0) {
+        return [NSString stringWithFormat:@"%lu km %lu m", (unsigned long)lengthInKilometers, (unsigned long)lengthInRemainder];
+    }
+    else{
+        return [NSString stringWithFormat:@"%lu m", (unsigned long)meters];
+    }
+}
+
++(NSString *)timediffFromSeconds:(NSUInteger)seconds {
+    NSUInteger mins = seconds/60;
+    if(mins < 1){
+        mins = 1;
+    }
+    if(mins < 60){
+        return [NSString stringWithFormat:@"%lum", (unsigned long)mins];
+    }else if(mins < 24*60){
+        return [NSString stringWithFormat:@"%luh", (unsigned long)mins/60];
+    }else{
+        NSUInteger hours = mins/60;
+        NSUInteger days = hours/24;
+        if(days < 31){
+            return [NSString stringWithFormat:@"%lud", (unsigned long)days];
+        }
+        return [NSString stringWithFormat:@"%luw", (unsigned long)days/7];
+    }
+    return @"";
+}
+
+@end
+
+
+@implementation NSDate (Formatting)
+
++ (NSDateFormatter *)dvg_dateFormatter {
+    static NSDateFormatter *dateFormatter;
+    if (!dateFormatter) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateFormat = @"dd MMMM, yyyy";
+    }
+    
+    return dateFormatter;
+}
+
++ (NSDateFormatter *)dvg_relativeDateFormatter
+{
+    static NSDateFormatter *dateFormatter;
+    if (!dateFormatter) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateFormatter.timeStyle = kCFDateFormatterNoStyle;
+        dateFormatter.doesRelativeDateFormatting = YES;
+    }
+    
+    return dateFormatter;
+}
+
++ (NSDateFormatter *)dvg_dateTimeFormatter
+{
+    static NSDateFormatter *dateTimeFormatter;
+    if (!dateTimeFormatter) {
+        dateTimeFormatter = [[NSDateFormatter alloc] init];
+        dateTimeFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateTimeFormatter.timeStyle = NSDateFormatterShortStyle;
+        dateTimeFormatter.doesRelativeDateFormatting = YES;
+    }
+    
+    return dateTimeFormatter;
+}
+
++ (BOOL)isSameDayWithDate1:(NSDate*)date1 date2:(NSDate*)date2 {
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    
+    unsigned unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth |  NSCalendarUnitDay;
+    NSDateComponents* comp1 = [calendar components:unitFlags fromDate:date1];
+    NSDateComponents* comp2 = [calendar components:unitFlags fromDate:date2];
+    
+    return [comp1 day]   == [comp2 day] &&
+    [comp1 month] == [comp2 month] &&
+    [comp1 year]  == [comp2 year];
+}
+
++ (NSString *)dvg_DateSince:(NSDate *)dt {
+    return [[NSDate dvg_dateFormatter] stringFromDate:dt];
+}
+
++ (NSString*)dvg_NiceDatestampSince:(NSDate*)dt
+{
+    if(dt == nil || [dt timeIntervalSince1970] < 365*24*60*60){
+        return NSLocalizedString(@"", nil);
+    }
+    NSDate* now = [NSDate date];
+    if([NSDate isSameDayWithDate1:now date2:dt]){
+        return NSLocalizedString(@"Today", nil);
+    }
+    return [[NSDate dvg_relativeDateFormatter] stringFromDate:dt];
+}
+
++ (NSString *)dvg_NiceTimesinceWithDate:(NSDate *)date {
+    if (!date) {
+        return @"";
+    }
+    NSDate* ts = [NSDate date];
+    if([ts timeIntervalSinceDate:date] < 2*60){
+        return @"a minute ago";
+    }
+    if([ts timeIntervalSinceDate:date] < 30*60){
+        return [NSString stringWithFormat:@"%i minutes ago", (int)[ts timeIntervalSinceDate:date]/60];
+    }
+    if([ts timeIntervalSinceDate:date] < 60*60){
+        return @"an hour ago";
+    }
+    return [NSDate dvg_NiceTimestampWithDate:date];
+}
+
++ (NSString *)dvg_NiceTimestampWithDate:(NSDate *)date {
+    if (!date) {
+        return @"";
+    }
+    
+    NSString *dateString = [NSDate dvg_NiceDatestampSince:date];
+    
+    
+    NSDateFormatterStyle dateStyle = [NSDate dvg_dateTimeFormatter].dateStyle;
+    [NSDate dvg_dateTimeFormatter].dateStyle = NSDateFormatterNoStyle;
+    [NSDate dvg_dateTimeFormatter].doesRelativeDateFormatting = NO;
+    [NSDate dvg_dateTimeFormatter].dateFormat = @"h:mm";
+    NSString *timeString = [[NSDate dvg_dateTimeFormatter] stringFromDate:date];
+    
+    [NSDate dvg_dateTimeFormatter].dateFormat = @"a";
+    NSString *periodString = [[NSDate dvg_dateTimeFormatter] stringFromDate:date];
+    
+    [NSDate dvg_dateTimeFormatter].dateFormat = nil;
+    [NSDate dvg_dateTimeFormatter].dateStyle = dateStyle;
+    [NSDate dvg_dateTimeFormatter].doesRelativeDateFormatting = YES;
+    
+    return [NSString stringWithFormat:@"%@, %@%@", dateString, timeString, [periodString uppercaseString]];
+}
+
+@end
