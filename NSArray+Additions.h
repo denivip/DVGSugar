db2a7458de5b2b7af6f6f43b400768cf73ad1605
IPv6 <ipv6@Nikolays-iMac.local>
2016-07-19 17:51:14 +0300

    map utility

--- NSArray+Additions.h
+++ NSArray+Additions.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 @interface NSArray (Additions)
 - (id)objectAtIndex:(NSInteger)index or:(id)defValue;
+- (NSArray *)mapObjectsUsingBlock:(id (^)(id obj, NSUInteger idx))block;
 
 @end
--- NSArray+Additions.m
+++ NSArray+Additions.m
@@ -12,4 +12,12 @@
     return res;
 }
 
+- (NSArray *)mapObjectsUsingBlock:(id (^)(id obj, NSUInteger idx))block {
+    NSMutableArray *result = [NSMutableArray arrayWithCapacity:[self count]];
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [result addObject:block(obj, idx)];
+    }];
+    return result;
+}
+
 @end
