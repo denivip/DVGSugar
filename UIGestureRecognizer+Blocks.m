f23433ba3a0d4e3c05e50385cbf404af71231666
IPv6 <ipv6@Nikolays-iMac.local>
2017-03-09 17:25:46 +0300

    auto-arc

--- UIGestureRecognizer+Blocks.m
+++ UIGestureRecognizer+Blocks.m
@@ -1,30 +1,30 @@
-  //  Copyright (c) 2011, Kevin O'Neill
-  //  All rights reserved.
-  //
-  //  Redistribution and use in source and binary forms, with or without
-  //  modification, are permitted provided that the following conditions are met:
-  //
-  //  * Redistributions of source code must retain the above copyright
-  //   notice, this list of conditions and the following disclaimer.
-  //
-  //  * Redistributions in binary form must reproduce the above copyright
-  //   notice, this list of conditions and the following disclaimer in the
-  //   documentation and/or other materials provided with the distribution.
-  //
-  //  * Neither the name UsefulBits nor the names of its contributors may be used
-  //   to endorse or promote products derived from this software without specific
-  //   prior written permission.
-  //
-  //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  //  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  //  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  //  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-  //  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  //  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  //  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  //  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  //  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  Copyright (c) 2011, Kevin O'Neill
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+//  * Neither the name UsefulBits nor the names of its contributors may be used
+//   to endorse or promote products derived from this software without specific
+//   prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "UIGestureRecognizer+Blocks.h"
 
@@ -40,42 +40,45 @@
 
 @implementation UIGestureRecognizer (Blocks)
 
-static char block_key; 
+static char block_key;
 
 + (id)instanceWithActionBlock:(void (^) (UIGestureRecognizer* gesture))action;
 {
-  id instance = [[[self class] alloc] initWithActionBlock:action];
-  
-  return [instance autorelease];
+    id instance = [[[self class] alloc] initWithActionBlock:action];
+#if !__has_feature(objc_arc)
+    return [instance autorelease];
+#else
+    return instance;
+#endif
 }
 
 - (id)initWithActionBlock:(void (^) (UIGestureRecognizer* gesture))action;
 {
-  if ((self = [self initWithTarget:self action:@selector(handleAction:)]))
-  {
-    [self setActionBlock:action];
-  }
-  
-  return self;
+    if ((self = [self initWithTarget:self action:@selector(handleAction:)]))
+    {
+        [self setActionBlock:action];
+    }
+    
+    return self;
 }
 
 - (void)handleAction:(UIGestureRecognizer *)recognizer;
 {
-  void (^action) (UIGestureRecognizer* gesture) = [self actionBlock];
-  if(nil != action)
-  {
-    action(recognizer);
-  }
+    void (^action) (UIGestureRecognizer* gesture) = [self actionBlock];
+    if(nil != action)
+    {
+        action(recognizer);
+    }
 }
 
 - (void (^) (UIGestureRecognizer* gesture))actionBlock;
 {
-  return objc_getAssociatedObject(self, &block_key);
+    return objc_getAssociatedObject(self, &block_key);
 }
 
 - (void)setActionBlock:(void (^) (UIGestureRecognizer* gesture))block;
 {
-  objc_setAssociatedObject(self, &block_key, block, OBJC_ASSOCIATION_COPY);
+    objc_setAssociatedObject(self, &block_key, block, OBJC_ASSOCIATION_COPY);
 }
 
 @end
