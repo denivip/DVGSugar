50603c728b4c589783e6b5f6a6af927587fbbf67
razinkov <razinkov@denivip.ru>
2019-02-21 17:50:34 +0300

    nsdata additions

---
+++ ClassesCommon/NSData+Utils.h
@@ -0,0 +1,17 @@
+//
+//  NSData + md5.h
+//  Transcribe
+//
+//  Created by Рябов Владислав on 08/11/2018.
+//  Copyright © 2018 DENIVIP Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSData(MD5)
+
+- (NSString*)MD5;
+
+@end
+
---
+++ ClassesCommon/NSData+Utils.m
@@ -0,0 +1,30 @@
+//
+//  NSData + md5.m
+//  Transcribe
+//
+//  Created by Рябов Владислав on 08/11/2018.
+//  Copyright © 2018 DENIVIP Group. All rights reserved.
+//
+
+#import "NSData+Utils.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSData(MD5)
+
+- (NSString*)MD5
+{
+    // Create byte array of unsigned chars
+    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
+
+    // Create 16 byte MD5 hash value, store in buffer
+    CC_MD5(self.bytes, (unsigned int)self.length, md5Buffer);
+
+    // Convert unsigned char buffer to NSString of hex values
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x",md5Buffer[i]];
+
+    return output;
+}
+
+@end
