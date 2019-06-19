e27215cd7d99d3f81924d3d98c20df3d56d510ab
Ilja Razinkov <razinkov@gmail.com>
2019-06-19 13:12:50 +0300

    Update NSUserDefaults+Utils.h

--- ClassesCommon/NSUserDefaults+Utils.h
+++ ClassesCommon/NSUserDefaults+Utils.h
@@ -1,11 +1,3 @@
-//
-//  NSString_Extended.h
-//  NineHundredSeconds
-//
-//  Created by Sergey Shpygar on 13.05.15.
-//  Copyright (c) 2015 DENIVIP Group. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface NSUserDefaults (Extended)
